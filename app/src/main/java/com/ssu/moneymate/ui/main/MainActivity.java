f5647282f925ec65ad02acb7d9652faa32f0c66b
yujin <jini1514@soongsil.ac.kr>
2023-10-06 12:47:19 +0900

    fix: 뷰페이저 고침

--- app/src/main/java/com/ssu/moneymate/ui/main/MainActivity.java
+++ app/src/main/java/com/ssu/moneymate/ui/main/MainActivity.java
@@ -1,24 +1,36 @@
 package com.ssu.moneymate.ui.main;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.ssu.moneymate.R;
 import com.ssu.moneymate.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater()); // 1
-        setContentView(binding.getRoot()); // 2
+        PagerAdapter viewPager2Adapter
+                = new PagerAdapter(getSupportFragmentManager(), getLifecycle());
+        ViewPager2 viewPager2 = findViewById(R.id.pager);
+        viewPager2.setAdapter(viewPager2Adapter);
 
-        VPAdapter adapter = new VPAdapter(getSupportFragmentManager());
-        binding.viewpager.setAdapter(adapter);
+        //=== TabLayout기능 추가 부분 ============================================
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText("Tab " + (position + 1));
+            }
+        }).attach();
+        //========================================================================
     }
 }
---
+++ app/src/main/java/com/ssu/moneymate/ui/main/PagerAdapter.java
@@ -0,0 +1,45 @@
+package com.ssu.moneymate.ui.main;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.ssu.moneymate.ui.main.fixed.FixedFragment;
+import com.ssu.moneymate.ui.main.goal.GoalFragment;
+import com.ssu.moneymate.ui.main.property.PropertyFragment;
+import com.ssu.moneymate.ui.main.solution.SolutionFragment;
+
+import java.util.ArrayList;
+
+public class PagerAdapter extends FragmentStateAdapter {
+
+    public PagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new PropertyFragment();
+            case 1:
+                return new FixedFragment();
+            case 2:
+                return new GoalFragment();
+            case 3:
+                return new SolutionFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4;       // 페이지 수
+    }
+}
