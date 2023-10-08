6c16a8c572577d82df16b4a8c67b7947f1e59a69
Amepistheo <kellylhji@gmail.com>
2023-10-08 23:10:41 +0900

    feat: 체크박스 기능 활성화 #5

--- app/src/main/java/com/ssu/moneymate/ui/main/property/BankFragment.java
+++ app/src/main/java/com/ssu/moneymate/ui/main/property/BankFragment.java
@@ -4,36 +4,65 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
 import com.ssu.moneymate.R;
+import com.ssu.moneymate.databinding.FragmentBankBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BankFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class BankFragment extends Fragment {
-    public static BankFragment newInstance(String param1, String param2) {
-        BankFragment fragment = new BankFragment();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
-        return fragment;
-    }
+        private FragmentBankBinding binding;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            if (getArguments() != null) {
+            }
         }
-    }
+
+        /*@Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            //return inflater.inflate(R.layout.fragment_bank, container, false);
+        }*/
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bank, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentBankBinding.inflate(inflater, container, false);
+
+        CheckBox kb_check = binding.check1;
+        CheckBox nh_check = binding.check2;
+        CheckBox nh_finance_check = binding.check3;
+
+        binding.btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // CheckBox 상태를 확인하여 필요한 정보를 저장할 리스트를 만듭니다.
+                List<String> selectedCheckBoxes = new ArrayList<>();
+
+                if (kb_check.isChecked()) {
+                    selectedCheckBoxes.add("KB 체크");
+                }
+                if (nh_check.isChecked()) {
+                    selectedCheckBoxes.add("NH 체크");
+                }
+                if (nh_finance_check.isChecked()) {
+                    selectedCheckBoxes.add("NH 금융 체크");
+                }
+
+                // 선택된 CheckBox 정보를 다른 곳으로 넘깁니다.
+                for (String checkBoxInfo : selectedCheckBoxes) {
+                    Log.d("CheckBoxInfo", checkBoxInfo);
+                }
+            }
+        });
+        return binding.getRoot();
     }
 }
