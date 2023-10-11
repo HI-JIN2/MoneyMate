f69e46d8d3caaaa4854cc8878bad65cbd8c86615
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2023-10-11 18:57:21 +0900

    [MOD/#10] 코드 주석 처리

--- app/src/main/java/com/ssu/moneymate/ui/main/data/source/ApiClient.java
+++ app/src/main/java/com/ssu/moneymate/ui/main/data/source/ApiClient.java
@@ -5,14 +5,14 @@ import com.ssu.moneymate.ui.main.data.service.ApiService;
 import retrofit2.Retrofit;
 
 public class ApiClient {
-    private static final String BASE_URL = "your_base_url_here";
-
-    public static ApiService createApiService() {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        return retrofit.create(ApiService.class);
-    }
+//    private static final String BASE_URL = "your_base_url_here";
+//
+//    public static ApiService createApiService() {
+//        Retrofit retrofit = new Retrofit.Builder()
+//                .baseUrl(BASE_URL)
+//                .addConverterFactory(GsonConverterFactory.create())
+//                .build();
+//
+//        return retrofit.create(ApiService.class);
+//    }
 }
