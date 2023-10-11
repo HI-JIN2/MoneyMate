6f1810f3d10cf2bc015c704e1990e7f67ed30754
yujin <jini1514@soongsil.ac.kr>
2023-10-11 23:46:01 +0900

    feat: 솔루션 없는 뷰에서 버튼 누르면 솔루션 뷰로 이동!

--- app/src/main/java/com/ssu/moneymate/ui/main/PagerAdapter.java
+++ app/src/main/java/com/ssu/moneymate/ui/main/PagerAdapter.java
@@ -11,6 +11,7 @@ import androidx.viewpager2.adapter.FragmentStateAdapter;
 import com.ssu.moneymate.ui.main.fixed.FixedFragment;
 import com.ssu.moneymate.ui.main.goal.GoalFragment;
 import com.ssu.moneymate.ui.main.property.PropertyFragment;
+import com.ssu.moneymate.ui.main.solution.NonSolutionFragment;
 import com.ssu.moneymate.ui.main.solution.SolutionFragment;
 
 import java.util.ArrayList;
@@ -32,7 +33,7 @@ public class PagerAdapter extends FragmentStateAdapter {
             case 2:
                 return new GoalFragment();
             case 3:
-                return new SolutionFragment();
+                return new NonSolutionFragment();
             default:
                 return null;
         }
--- app/src/main/java/com/ssu/moneymate/ui/main/solution/NonSolutionFragment.java
+++ app/src/main/java/com/ssu/moneymate/ui/main/solution/NonSolutionFragment.java
@@ -9,58 +9,44 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.ssu.moneymate.R;
+import com.ssu.moneymate.databinding.FragmentBankBinding;
+import com.ssu.moneymate.databinding.FragmentGoalBinding;
+import com.ssu.moneymate.databinding.FragmentNonSolutionBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NonSolutionFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class NonSolutionFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FragmentNonSolutionBinding binding;
 
     public NonSolutionFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NonSolutionFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NonSolutionFragment newInstance(String param1, String param2) {
-        NonSolutionFragment fragment = new NonSolutionFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_non_solution, container, false);
+
+        binding = FragmentNonSolutionBinding.inflate(inflater, container, false);
+
+        binding.btnRecreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFragment(view);
+            }
+        });
+        return binding.getRoot();
+    }
+
+    // 이 메서드는 버튼을 클릭할 때 호출됩니다.
+    public void openFragment(View view) {
+        // 두 번째 프래그먼트를 열기 위한 트랜잭션을 시작합니다.
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.layout_non, new SolutionFragment())
+                .addToBackStack(null)  // 이전 프래그먼트 스택에 추가
+                .commit();
     }
 }
