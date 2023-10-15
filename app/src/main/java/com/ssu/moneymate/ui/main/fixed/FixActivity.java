92c994ca53131d0f349a8f7e3e05e5578ccca2c4
binisnil <dbsqlssss0704@gmail.com>
2023-10-15 20:51:49 +0900

    [Feat] #20 RoomDB 이용하여 데이터 저장 완료

--- app/src/main/java/com/ssu/moneymate/ui/main/fixed/FixActivity.java
+++ app/src/main/java/com/ssu/moneymate/ui/main/fixed/FixActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.RadioButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,17 +12,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.ssu.moneymate.R;
 import com.ssu.moneymate.databinding.ActivityFixedBinding;
 
+import java.util.ArrayList;
 import java.util.zip.Inflater;
+
 
 public class FixActivity extends AppCompatActivity {
 
     private ActivityFixedBinding binding;
+    private String[] categoryList = {"선택없음", "생활", "저축","식비","교통","기타"};
+    RoomDB database;
+//    ArrayList<FixedData> dataList = new ArrayList<>;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityFixedBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        database = RoomDB.getInstance(this);
 
         TextWatcher textWatcher = new TextWatcher() {
             @Override
@@ -60,6 +68,18 @@ public class FixActivity extends AppCompatActivity {
         binding.btnFixSettingComplete.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+
+                int categoryId = binding.radioFixCategory.getCheckedRadioButtonId();
+                RadioButton selectedRadioButton = findViewById(categoryId);
+                String category = selectedRadioButton.getText().toString();
+                String money = binding.etFixMoney.getText().toString().trim();
+
+                if (!money.equals("")) {
+                    FixedData data = new FixedData();
+                    data.setCategory(category);
+                    data.setMoney(money);
+                    database.fixedDao().insert(data);
+                }
                 finish();
             }
         });
--- app/src/main/java/com/ssu/moneymate/ui/main/fixed/FixedData.java
+++ app/src/main/java/com/ssu/moneymate/ui/main/fixed/FixedData.java
@@ -1,8 +1,22 @@
 package com.ssu.moneymate.ui.main.fixed;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
-public class FixedData {
+import java.io.Serializable;
+
+@Entity(tableName = "table_name")
+public class FixedData implements Serializable{
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    @ColumnInfo(name = "category")
     String category;
+    @ColumnInfo(name = "money")
     String money;
+
+    public int getId() {
+        return id;
+    }
 
     public String getCategory() {
         return category;
@@ -12,8 +26,21 @@ public class FixedData {
         return  money;
     }
 
-    public  FixedData(String category, String money){
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCategory(String category) {
         this.category = category;
+    }
+
+    public void setMoney(String money) {
         this.money = money;
     }
+
+    //    public  FixedData(int id, String category, String money){
+//        this.id = id;
+//        this.category = category;
+//        this.money = money;
+//    }
 }
